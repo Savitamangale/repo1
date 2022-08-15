3e975dbd5af7e0badeb9f43cd083a826ffc97d44
Savitamangale <sakohre@gmail.com>
2022-08-15 23:08:23 +0530

    commit

---
+++ src/abc/Demo.java
@@ -0,0 +1,18 @@
+package abc;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		byte a=100;
+		System.out.println(a);
+		int b=25;
+		int c=95;
+		int d=b-c;
+		ystem.out,println(d);
+		
+
+	}
+
+}
